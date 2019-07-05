521b6512438014cd77fade460d58404c49a18899
Kunal Vohra <kunalvohra94@gmail.com>
2019-07-05 16:47:51 +0530

    updated index.js for options

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 #! /usr/bin/env node
 const program = require('commander');
-var crawler = require('./crawler');
 
-program
-  .version('0.1.0')
 
+var criclnk = "https://www.cricbuzz.com/cricket-match/live-scores";
+var options = [];
 program
-    .command("live")
-    .alias("l")
-    .action(()=>{
-        crawler
-    })
+.version('1.1.0')
+.option('-l, --live', 'Live')
+.option('-c, --commentary', 'Commentary')
+.option('-s, --scorecard', 'Scorecard')
+
+.parse(process.argv);
+
+if (program.live || program.all) 
+{
+  options.push("l");
+}
+
+if (program.commentary || program.all) 
+{
+  options.push("c");
+
+}
+if (program.scorecard || program.all) 
+{
+  options.push("s");
+
+}
+if (!program.live && !program.commentary && !program.scorecard && !program.all)
+{
+  options.push("l");
+}
+
+var MatchIndex =  require('./MatchIndex')
+MatchIndex.crawlerFunction(criclnk, options);
 
 program.parse(process.argv);
