6decfa1a402d7c29eee68231464499be5f9fde15
sharmarajat01 <rajat.sharma0819@gmail.com>
2019-07-05 16:52:50 +0530

    added options

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ program
 .option('-l, --live', 'Live')
 .option('-c, --commentary', 'Commentary')
 .option('-s, --scorecard', 'Scorecard')
+.option('-u, --umpires', 'Umpires')
+.option('-r, --recent', 'Recent')
+.option('-t, --toss', 'Toss')
+.option('-a, --all', 'All')
 
 .parse(process.argv);
 
@@ -27,7 +31,21 @@ if (program.scorecard || program.all)
   options.push("s");
 
 }
-if (!program.live && !program.commentary && !program.scorecard && !program.all)
+if (program.umpires || program.all) 
+{
+  options.push("u");
+}
+if (program.recent || program.all) 
+{
+  options.push("r");
+
+}
+if (program.toss || program.all)
+{
+  options.push("t");
+
+}
+if (!program.live && !program.commentary && !program.scorecard && !program.umpires && !program.recent && !program.toss && !program.all)
 {
   options.push("l");
 }
